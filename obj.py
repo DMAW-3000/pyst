459b2356cec659fd606b9c620513752d6b908c64
DMAW-3000 <thewood005@gmail.com>
2025-05-09 11:28:27 -0400

    Added alternate linear Object id generator

--- obj.py
+++ obj.py
@@ -3,11 +3,10 @@ Object ID manager
 """
 
 import random
-
 random.seed()
 
 
-class OBJ_TABLE(object):
+class OBJ_TABLE_BASE(object):
     """
     Maintain a unique ID for every Object in the system
     """
@@ -15,6 +14,20 @@ class OBJ_TABLE(object):
     _Obj_Map = set()
     _Min_Id = 3
     _Max_Id = 0xffffffff
+    
+    @classmethod
+    def free_obj(klass, objId):
+        """
+        Remove the Object id from the global set.
+        """
+        klass._Obj_Map.discard(objId)
+    
+
+class OBJ_TABLE_RANDOM(OBJ_TABLE_BASE):
+    """
+    Maintain a unique ID for every Object in the system
+    """
+    
     _Get_Random = random.randrange
 	
     @classmethod
@@ -30,10 +43,26 @@ class OBJ_TABLE(object):
         objMap.add(objId)
         return objId
         
-    @classmethod
-    def free_obj(klass, objId):
-        """
-        Remove the Object id from the global set.
-        """
-        klass._Obj_Map.discard(objId)
+        
+class OBJ_TABLE_LINEAR(OBJ_TABLE_BASE):
 
+    _Cur_Id = OBJ_TABLE_BASE._Min_Id
+
+    @classmethod
+    def new_obj(klass):
+        """
+        Allocate a new ID for the given Object.
+        Returns the ID value.
+        """
+        objMap = klass._Obj_Map
+        objId = klass._Cur_Id
+        while objId in objMap:
+            objId += 7
+            if objId > klass._Max_Id:
+                objId = klass._Min_Id
+        objMap.add(objId)
+        return objId
+
+
+# globals
+OBJ_TABLE = OBJ_TABLE_LINEAR
