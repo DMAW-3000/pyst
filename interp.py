94e7bef909adcea3c2b9b9f53626246deeefe8ad
DMAW-3000 <thewood005@gmail.com>
2025-05-10 12:19:27 -0400

    Add reset() method

--- interp.py
+++ interp.py
@@ -14,17 +14,31 @@ class Interp(object):
         """
         Create a new interpeter
         """
+        # cache values from system
         self._sys = system
         self._nil = system.o_nil
         
+        # the interpreter global state
         self.i_this = self._nil
         self.i_context = self._nil
         self.i_method = self._nil
         self.i_temps = self._nil
         self.i_literals = self._nil
         
-    def run(self):
+        # the bytecode table
+        self.b_table = bTbl = [None] * 256
+        
+    def reset(self):
+        """
+        Prepare for execution
+        """
+        self.i_context = BlockContext(self._nil)
+        
+    def exe(self):
         """
         Start execution
         """
-        self.i_context = BlockContext(self._nil)
+        oldCtx = self.i_context
+        newCtx = MethodContext(oldCtx)
+        
+        
