ac956914b1fd8e4409e365a36454aba467233edf
DMAW-3000 <thewood005@gmail.com>
2025-05-10 10:56:22 -0400

    Start using argparser library for command line

--- pyst.py
+++ pyst.py
@@ -6,5 +6,12 @@ from system import Smalltalk
 
 if __name__ == '__main__':
 
+    from argparse import ArgumentParser
+    
+    parser = ArgumentParser(prog = "pyst.py",
+                            description = "A Smalltalk environment implemented in python")
+    #parser.add_argument("img_file", type=str, help = "system state image file")
+    args = parser.parse_args()
+
     Smalltalk.rebuild()
     
