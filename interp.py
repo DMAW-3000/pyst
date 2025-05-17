2721dda679561f20eeb80daff84c04ff327968e2
DMAW-3000 <thewood005@gmail.com>
2025-05-17 11:05:38 -0400

    First try at send_message() method.  Stopping for now because I needed identity dictionaries to handle selectory method lookup

--- interp.py
+++ interp.py
@@ -19,12 +19,7 @@ class Interp(object):
         self._nil = system.o_nil
         
         # the interpreter global state
-        self.i_this = self._nil
         self.i_context = self._nil
-        self.i_method = self._nil
-        self.i_temps = self._nil
-        self.i_literals = self._nil
-        self.i_self = self._nil
         
         # the bytecode table
         self.b_table = bTbl = [None] * 256
@@ -33,9 +28,54 @@ class Interp(object):
         """
         Prepare for execution
         """
-        self.i_self = self._nil
-        self.i_context = self._nil
-        self.i_method = self._nil
+        # create the root context
+        # leave the parent nil
+        self.i_context = MethodContext()
+        
+    def send_message(self, numArgs):
+        """
+        Send a message.  This assumes that the receiver,
+        message selector, and argument values have been
+        pushed to the current stack.
+        """
+        oldCtx = self.i_context
+        pop = oldCtx.pop
+        
+        # pop the message arguments
+        argList = []
+        n = numArgs
+        while n > 0:
+            argList.append(pop())
+            n -= 1
+        argList.reverse()
+        print(argList)
+        
+        # pop the message selector
+        selObj = pop()
+        print(selObj)
+        
+        # pop the receiver
+        recvObj = pop()
+        print(recvObj)
+        
+        # TODO: lookup method object
+        methDict = recvObj.get_class().methodDictionary
+        
+        # check number of arguments
+        if numArgs != methObj.get_num_arg():
+            raise RuntimeError("wrong number of arguments")
+        
+        # allocate a new context and link to old
+        newCtx = MethodContext()
+        newCtx.parent = oldCtx
+        newCtx.receiver = recvObj
+        newCtx.method = methObj
+        
+        # transfer control to new context
+        self.i_context = newCtx
+
+        
+        
         
         
         
