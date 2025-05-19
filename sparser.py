73a4355597bad288283711fc8c2c4327a39d427f
DMAW-3000 <thewood005@gmail.com>
2025-05-19 15:27:58 -0400

    Add docstrings

--- sparser.py
+++ sparser.py
@@ -8,48 +8,84 @@ from lexer import tokens
 
 
 class ParseStatementList(object):
+    """
+    Represent a list of statements.
+    """
     def __init__(self, x):
         self.data = x
 
 class ParseStatement(object):
+    """
+    Represent a single statement
+    """
     def __init__(self, x):
         self.data = x
 
-class ParseExecStatement(ParseStatement): pass
+class ParseExecStatement(ParseStatement):
+    """
+    Represent a statement with no return
+    or assign.
+    """
+    pass
 
-class ParseReturnStatement(ParseStatement): pass
+class ParseReturnStatement(ParseStatement):
+    """
+    Represent a return statement
+    """
+    pass
 
 class ParseAssignStatement(ParseStatement):
+    """
+    Represent an assignment statement
+    """
     def __init__(self, var, x):
         super().__init__(x)
         self.var = var
 
 class ParseMessage(object):
+    """
+    Represent a message to a receiver
+    """
     def __init__(self, recv):
         self.recv = recv
 
 class ParseUnaryMessage(ParseMessage):
+    """
+    Represent a unary message
+    """
     def __init__(self, recv, name):
         super().__init__(recv)
         self.name = name
         
 class ParseExprMessage(ParseMessage):
+    """
+    Represent a binary expression message
+    """
     def __init__(self, recv, name, send):
         super().__init__(recv)
         self.name = name
         self.send = send
         
 class ParseArgumentMessage(ParseMessage):
+    """
+    Represent an argument list message
+    """
     def __init__(self, recv, args):
         super().__init__(recv)
         self.args = args
         
 class ParseMessageArg(object):
+    """
+    Represent a message argument/value pair
+    """
     def __init__(self, name, value):
         self.name = name
         self.value = value
 
 class ParseLiteral(object):
+    """
+    Represent a literal value
+    """
     def __init__(self, x):
         self.value = x
         
