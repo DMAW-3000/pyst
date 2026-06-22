a20d6aefdf09661f9f93c8724708f7720318c80c
DMAW-3000 <thewood005@gmail.com>
2026-06-22 14:31:14 -0400

    Remove debug print

--- compiler.py
+++ compiler.py
@@ -488,7 +488,6 @@ class Compile(object):
         self.compile_exec_statement(recv.data)
         sym = self._sys.symbol_find_or_add(name)
         idx = self.add_literal(sym)
-        print("compile msg: ", name)
         self.emit_bytes(B_PUSH_LIT_CONSTANT, idx)
         self.compile_exec_statement(send.data)
         self.emit_bytes(B_SEND, 1)
