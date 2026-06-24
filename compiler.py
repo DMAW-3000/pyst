8f08db25685fe4247837cca93f211247fc3b1b46
DMAW-3000 <thewood005@gmail.com>
2026-06-24 11:11:27 -0400

    Add Array of inst var name Strings to class definition

--- compiler.py
+++ compiler.py
@@ -192,6 +192,11 @@ class Compile(object):
             tok = self._lex.token()
         if (not tok.type == "OPERATOR") or (not tok.value == '|'):
             raise CompileError("inst vars missing |")
+       
+        self._cur_klass.instanceVariables = Array(len(self._cur_inst_var))
+        for n,s in enumerate(self._cur_inst_var):
+            self._cur_klass.instanceVariables[n] = String.from_str(s)
+        #self._sys.arr_print(self._cur_klass.instanceVariables)
         
         
     def parse_class_var(self, varName):
