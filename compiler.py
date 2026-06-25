0603bf6216a7bd93befccb609b86caf1b165c5f2
DMAW-3000 <thewood005@gmail.com>
2026-06-24 22:23:01 -0400

    Set block header values when compiling

--- compiler.py
+++ compiler.py
@@ -627,6 +627,7 @@ class Compile(object):
         
         # create new block object and its literals array
         blkObj = CompiledBlock()
+        blkObj.set_hdr(0, self._cur_depth)
         blkObj.set_code(self._cur_bytes)
         blkObj.literals = Array.from_seq(self._cur_literal)
         blkObj.method = self._cur_meth
