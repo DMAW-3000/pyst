60447b399682f320496ce57568716c571f6e986b
DMAW-3000 <thewood005@gmail.com>
2026-08-07 20:11:03 -0400

    Add ArithmeticError and ZeroDivide classes to kernel

--- system.py
+++ system.py
@@ -36,6 +36,7 @@ class Smalltalk(object):
         self.k_unhandled_except         = None
         self.k_error                    = None
         self.k_notification             = None
+        self.k_arith_error              = None
         self.k_invalid_value            = None
         self.k_not_indexable            = None
         self.k_read_only_obj            = None
@@ -43,6 +44,7 @@ class Smalltalk(object):
         self.k_invalid_arg              = None
         self.k_arg_range                = None
         self.k_index_range              = None
+        self.k_zero_divide              = None
         self.k_end_of_stream            = None
         self.k_message                  = None
         self.k_behavior                 = None
