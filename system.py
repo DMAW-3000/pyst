2cd808830bd42f26c0dc56928bf60dcb6dd520af
DMAW-3000 <thewood005@gmail.com>
2026-08-12 17:54:49 -0400

    Add SortedCollection class to kernel

--- system.py
+++ system.py
@@ -62,6 +62,7 @@ class Smalltalk(object):
         self.k_seq_collection           = None
         self.k_arr_collection           = None
         self.k_ord_collection           = None
+        self.k_sort_collection          = None
         self.k_array                    = None
         self.k_bytearray                = None
         self.k_link                     = None
